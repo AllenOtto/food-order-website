e26921561a8d363a9d2db569046d81295af8ffc2
AllenOtto <kephamoturi@gmail.com>
2021-11-30 23:34:14 +0300

    Updated Admin management page with button to add admins, admins table with dummy text

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,10 +3,40 @@
     <!-- Main Content Section Starts -->
     <div class="main-content">
         <div class="wrapper">
-            <h1>Manage Admin</h1>
+            <h1>Manage Admins</h1><br><br>
+            
+            <!-- Button to add Admin -->
+            <a href="#" class="btn-primary">Add Admin</a><br><br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>Serial #</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Allen Moturi</td>
+                    <td>AllenO</td>
+                    <td>Update Admin Delete Admin</td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Wendy Moturi</td>
+                    <td>WendyM</td>
+                    <td>Update Admin Delete Admin</td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Wilder Moturi</td>
+                    <td>WilderM</td>
+                    <td>Update Admin Delete Admin</td>
+                </tr>
+            </table>
 
             <div class="clearfix"></div>
-        </div>        
+        </div>
     </div>
     <!-- Main Content Section Ends -->
 
