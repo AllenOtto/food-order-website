7444b27c5871042ccb190eb2a12fc96c57da137a
AllenOtto <kephamoturi@gmail.com>
2021-11-30 16:07:29 +0300

    Added management page for admin, 'Manage Admin'

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -8,8 +8,8 @@
         <div class="menu text-center">
             <div class="wrapper">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Admin</a></li>
                     <li><a href="#">Category</a></li>
                     <li><a href="#">Food</a></li>
                     <li><a href="#">Order</a></li>
@@ -21,38 +21,18 @@
         <!-- Main Content Section Starts -->
         <div class="main-content">
             <div class="wrapper">
-                <h1>Dashboard</h1>
-                
-                <div class="col-4 text-center">
-                    <h1>Pastries</h1><br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>Dishes</h1><br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>Deserts</h1><br>
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>Drinks</h1><br>
-                    Categories
-                </div>
+                <h1>Manage Admin</h1>
 
                 <div class="clearfix"></div>
             </div>
-        
+                
         </div>
         <!-- Main Content Section Ends -->
 
         <!-- Footer Section Starts -->
         <div class="footer">
             <div class="wrapper">
-                <p class="text-center">&copy2020. All Rights Reserved, Wendy's Fast Foods. Developed By <a href="#"><b>BitWilderInc</b></a></p>
+                <p class="text-center">&copy2020. All Rights Reserved, Wendy's Fast Foods. Developed By <a href="index.html"><b>BitWilderInc</b></a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
