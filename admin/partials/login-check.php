22890aefd09e344b032b4561b1fb4c3bf3f963bb
AllenOtto <kephamoturi@gmail.com>
2021-12-13 00:08:47 +0300

    Included login-check.php in menu.php partial

--- admin/partials/login-check.php
+++ admin/partials/login-check.php
@@ -1,5 +1,10 @@
-// Authorization - Access Control
-// Check whether the user is logged in or not
-if(isset($_SESSION['user'])) {
-    
-}
+<?php
+    // Authorization - Access Control
+    // Check whether the user is logged in or not
+    if(!isset($_SESSION['user'])) {
+        // User is not logged in.
+        // Redirect to login page with message
+        $_SESSION['login-fail'] = "<div class='error'>Login Failed. Try Again</div>";
+        header('location:'.SITEURL.'admin/login.php');
+    }
+?>
