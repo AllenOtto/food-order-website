f6f09518519b90201a72558e5e8d1232b9d05434
AllenOtto <kephamoturi@gmail.com>
2021-12-26 23:50:04 +0300

    Deleted link to current image and replaced it with new one

--- admin/update-food.php
+++ admin/update-food.php
@@ -27,11 +27,11 @@
                         $row = mysqli_fetch_assoc($res);
 
                         // There's data so get individual data items
-                        $id = $row['id'];
+                        $id_db = $row['id'];
                         $title = $row['title'];
                         $description = $row['description'];
                         $price = $row['price'];
-                        $image_name = $row['image_name'];
+                        $current_image = $row['image_name'];
                         $category_id = $row['category_id'];
                         $featured = $row['featured'];
                         $active = $row['active'];
@@ -57,7 +57,9 @@
                     </tr>
                     <tr>
                         <td>Current Image</td>
-                        <td><img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" name="current-image" width="100px"></td>
+                        <td>
+                            <img src="<?php echo SITEURL; ?>images/food/<?php echo $current_image; ?>" width="100px">
+                        </td>
                     </tr>
                     <tr>
                         <td>Image</td>
@@ -67,7 +69,30 @@
                         <td>Category</td>
                         <td>
                             <select name="category">
-                                <option value="1">pork</option>
+                                <?php
+                                    // Create query to get all categories where active = "Yes"
+                                    $sql2 = "SELECT * FROM tbl_category WHERE active='Yes'; ";
+                                    // Execute query
+                                    $res2 = mysqli_query($conn, $sql2);
+                                    // Check if there is data
+                                    $count2 = mysqli_num_rows($res2);
+                                    if($count2>0) {
+                                        // Get data as an associative array for all data in database and populate table
+                                        while($row2=mysqli_fetch_assoc($res2)) {
+                                            // Get data items
+                                            $id_category = $row2['id'];
+                                            $title_category = $row2['title'];
+
+                                            ?>
+                                                <option value="<?php echo $id_category; ?>"><?php echo $title_category; ?></option>
+                                            <?php
+
+                                        }
+                                        
+                                    }
+
+                                ?>
+                                
                             </select>
                         </td>
                     </tr>
@@ -87,11 +112,106 @@
                     </tr>
                     <tr>
                         <td colspan="2">
-                            <input type="submit" value="Update Food" class="btn-secondary">
+                            <input type="hidden" name="id-db" value="<?php echo $id_db ?>">
+                            <input type="hidden" name="current-image" value="<?php echo $current_image; ?>">
+                            <input type="submit" name="submit" value="Update Food" class="btn-secondary">
                         </td>
                     </tr>
                 </table>
             </form>
+
+            <?php
+
+                if(isset($_POST['submit'])) {
+                    // Passed Hidden
+                    $id_db = $_POST['id-db'];
+                    $current_image = $_POST['current-image'];
+
+                    // Get the rest of the data items
+                    $title = $_POST['title'];
+                    $description = $_POST['description'];
+                    $price = $_POST['price'];
+                    $category_id = $_POST['category'];
+                    $featured = $_POST['featured'];
+                    $active = $_POST['active'];
+
+                    if(isset($_FILES['image']['name'])) {
+                        $image_name = $_FILES['image']['name'];
+                        // Check if we have a new image selected so as to replace current image
+                        if($image_name != "") { // New image is selected
+                            // Check if current image variable holds an image we should replace or not
+                            if($current_image != "") {
+                                // We have an old image we need to delete
+                                // We need its path and name
+                                $path = "../images/food/".$current_image;
+                                // Delete it
+                                $remove = unlink($path);
+                                // Check if it failed to delete, set a session error message, 
+                                // redirect food manage page and stop the process
+                                if($remove==false) {
+                                    $_SESSION['failed-to-delete-current-image'] = "<div class='error'>Failed to Delete Current Image</div>";
+                                    header('location:'.SITEURL.'admin/manage-food.php');
+                                    die();
+                                }
+
+                            }
+
+                        } else {
+                            // If no new image is set, set image name variable to empty
+                            $image_name = $current_image;
+                        }
+
+                        // Since there is a new image, rename it (randomizing it) and upload it
+                        // Fetch image extension for reuse
+                        $ext = end(explode(".", $image_name));
+
+                        $image_name = "Food_Item_".rand(0000,9999).".".$ext;
+
+                        // To upload it we need its source path, its destination path and its name
+                        $source_path = $_FILES['image']['tmp_name'];
+
+                        $destination_path = "../images/food/".$image_name;
+
+                        $upload = move_uploaded_file($source_path, $destination_path);
+
+                        // Check if image upload failed.
+                        // If it did, redirect to food manage page with error message and stop the process
+                        if($upload==false) {
+                            $_SESSION['new-image-upload-failed'] = "<div class='error'>Image Upload Failed</div>";
+                            header('location:'.SITEURL.'admin/manage-food.php');
+                            die();
+                        }
+
+                    } else {
+                        // If no new image is uploaded retain previous image
+                        $image_name = $current_image; // If there was no current image then it's still okay
+                    }
+
+                    // Create an sql query to update database
+                    $sql3 = "UPDATE tbl_food SET
+                        title='$title',
+                        description='$description',
+                        price='$price',
+                        image_name='$image_name',
+                        category_id='$category_id',
+                        featured='$featured',
+                        active='$active'
+                        WHERE id=$id_db;
+                    ";
+
+                    // Execute query
+                    $res3 = mysqli_query($conn, $sql3);
+                    // Check whether the query executed successfully
+                    if($res3==True) {
+                        echo "Success";
+                    } else {
+                        echo "Failed";
+                    }
+
+                }
+
+            ?>
+
         </div>
     </div>
 
